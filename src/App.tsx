5fd54ec34de394e14c91ea4cb3e0482541ca23f2
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-05-27 20:10:04 +0600

    :woozy_face: ctrl+s issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdfs";
+const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdf";
 
 const App = () => <embed width="100%" height="100%" src={resumeUrl} type="application/pdf"/>;
 
