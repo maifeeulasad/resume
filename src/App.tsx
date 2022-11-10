6699b110646ab0deb82997316d50a02b7d3a0bc5
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-11-10 21:27:07 +0600

    [fix-resume]: HSC 5.05->5.00

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdf";
 // contains info about Endimension
-const resumeUrl = "https://maifeeulasad.github.io/assets/resume/Resume-Maifee_Ul_Asad-Single_Page-2022_09_12.pdf";
+const resumeUrl = "https://maifeeulasad.github.io/assets/resume/Resume-Maifee_Ul_Asad-Single_Page-2022_11_10.pdf";
 
 const App = () => <embed width="100%" height="100%" src={resumeUrl} type="application/pdf"/>;
 
