b95e7c100c618a134992c3b5b1d427091ac9220c
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-11-24 05:45:23 +0600

    [enhancement]: pdf viewer for mobile & desktop; cleanup;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { osCheck, OsType } from "os-check";
+import { DesktopResume } from "./component/resume/DesktopResume";
+import { MobileResume } from "./component/resume/MobileResume";
 
 // const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdf";
 // contains info about Endimension
-const resumeUrl = "https://maifeeulasad.github.io/assets/resume/Resume-Maifee_Ul_Asad-Single_Page-2022_11_10.pdf";
+const resumeUrl =
+  "https://maifeeulasad.github.io/assets/resume/Resume-Maifee_Ul_Asad-Single_Page-2022_11_10.pdf";
 
-const App = () => <embed width="100%" height="100%" src={resumeUrl} type="application/pdf"/>;
+const App = () => {
+  const isDesktop = () => {
+    const os = osCheck();
+
+    return os === OsType.Linux || os === OsType.MacOS || os === OsType.Windows;
+  };
+
+  if (isDesktop()) {
+    return <DesktopResume resumeUrl={resumeUrl} />;
+  } else {
+    return <MobileResume resumeUrl={resumeUrl} />;
+  }
+};
 
 export default App;
