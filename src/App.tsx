acc2c5058daa52faffd27f0cc84f2125e5d428d5
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2023-02-02 18:02:06 +0600

    //wip

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { MobileResume } from "./component/resume/MobileResume";
 
 // const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdf";
 // contains info about Endimension
+// contains info about AppFlow
 const resumeUrl =
   "https://maifeeulasad.github.io/assets/resume/Maifee%20Ul%20Asad%20-%20Resume%20-%202023.02.02.pdf";
 
