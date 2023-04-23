739d01c73bc4ce9c9831f02a89c70a4467a88520
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-04-23 13:29:49 +0600

    [chores]: refactor filename;

--- scripts/download-pdfjs-cdn.js
+++ scripts/download-pdfjs-cdn.js
@@ -2,6 +2,7 @@ const https = require('https');
 const fs = require('fs');
 
 const url = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.5.141/pdf.worker.js";
+const fileName = "pdf.worker.js";
 
 function downloadFile(url, filename) {
   const file = fs.createWriteStream(filename);
@@ -13,4 +14,4 @@ function downloadFile(url, filename) {
   });
 }
 
-downloadFile(url, "pdf.worker.js");
+downloadFile(url, fileName);
