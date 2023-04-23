661bb1aae82edb181b49b6317ebfdac8017c724f
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-04-23 11:48:52 +0600

    [pdfjs]: added `postinstall` script to download `pdf.worker.js` from CDN

---
+++ scripts/download-pdfjs-cdn.js
@@ -0,0 +1,16 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.5.141/pdf.worker.js";
+
+function downloadFile(url, filename) {
+  const file = fs.createWriteStream(filename);
+  https.get(url, (response) => {
+    response.pipe(file);
+  });
+  file.on('finish', () => {
+    file.close();
+  });
+}
+
+downloadFile(url, "pdf.worker.js");
