e50bfbcc95ea6280335170b2c3eacebc6a813061
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2023-11-16 10:46:18 +0600

    [update]: updated resume source;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { PdfViewer } from "./component/pdf/PdfViewer";
 
-// const resumeUrl = "https://maifeeulasad.github.io/assets/resume/resume_maifeeulasad.pdf";
 // contains info about Endimension
 // contains info about AppFlow
-const resumeUrl = "https://maifeeulasad.github.io/assets/resume/Maifee%20Ul%20Asad%20-%20Resume%20-%202023.02.02.pdf";
+const resumeUrl = "https://maifeeulasad.github.io/assets/resume/maifeeulasad-resume.pdf";
 
 const App = () => {
   return (
